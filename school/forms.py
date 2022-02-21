f60c21507bb94c3ce16ff5780a0f753ef783b5b5
oshikadhungana7 <oshikadhungana798@gmail.com>
2022-02-21 06:07:12 -0800

    commit

--- school/forms.py
+++ school/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 from . import models
 
-#for admin
+
 class AdminSigupForm(forms.ModelForm):
     class Meta:
         model=User
         fields=['first_name','last_name','username','password']
 
 
-#for student related form
+
 class StudentUserForm(forms.ModelForm):
     class Meta:
         model=User
